36e56b1f91538df7dd58ce42f82f809e85b2317d
Vacationwkx <wkx611@outlook.com>
2022-02-22 18:50:29 +0800

    plot update

--- output/result_plot/seaborn_plot.py
+++ output/result_plot/seaborn_plot.py
@@ -127,9 +127,113 @@ def train_ppo():
     save_sns_plot(sns_plot,fig_title)
     print("done")
 
+def train_ppo_ddpg_sep():
+    fig_title = "PPO_DDPG_SEP"
+
+    dir_name = 'ppo_test_amd'
+    case_names = ['2021-12-19_15-00-57-ppo_s0_outliner','2021-12-21_11-09-31-ppo_s0_outliner']
+    ppo_file_name = []
+    for case_name in case_names:
+        ppo_file_name += get_csv(dir_name,case_name)
+    
+    dir_name = 'ddpg_test_amd'
+    case_names = ['2021-12-19_10-11-01-ddpg_s0_t5', '2021-12-19_01-15-44-ddpg_s0_should_t5']
+    ddpg_file_name = []
+    for case_name in case_names:
+        ddpg_file_name += get_csv(dir_name,case_name)
+
+    ppo_data  = get_min_data(ppo_file_name,set_min=250)
+    ddpg_data = get_min_data(ddpg_file_name,set_min=250)
+    data = np.c_[ppo_data,ddpg_data]
+    new_data = pd.DataFrame(data=data,index=np.arange(data.shape[0]), columns=['PPO1','PPO2','DDPG1','DDPG2'])
+    #new_data['index'] = new_data.index
+      
+    sns.set_theme(style='darkgrid')
+    plt.figure(figsize=(7,5))
+    sns_plot=sns.lineplot(data=new_data)
+    sns_plot.set_xlabel("Epochs")                        
+    sns_plot.set_ylabel("Reward")
+    sns_plot.set(title="Training Curve for PPO and DDPG")
+    sns_plot.figure.savefig( os.path.join(os.path.dirname( __file__ ),f"{fig_title}.png"))
+    print("done")
+
+def train_ppo_ddpg_group():
+    fig_title = "PPO_DDPG_GROUP"
+
+    dir_name = 'ppo_test_amd'
+    case_names = ['2021-12-19_15-00-57-ppo_s0_outliner','2021-12-21_11-09-31-ppo_s0_outliner']
+    ppo_file_name = []
+    for case_name in case_names:
+        ppo_file_name += get_csv(dir_name,case_name)
+    
+    dir_name = 'ddpg_test_amd'
+    case_names = ['2021-12-19_10-11-01-ddpg_s0_t5', '2021-12-19_01-15-44-ddpg_s0_should_t5']
+    ddpg_file_name = []
+    for case_name in case_names:
+        ddpg_file_name += get_csv(dir_name,case_name)
+
+    ppo_data  = get_min_data(ppo_file_name,set_min=250)
+    ddpg_data = get_min_data(ddpg_file_name,set_min=250)
+    ppo_df = pd.DataFrame(data=ppo_data)
+    ddpg_df = pd.DataFrame(data=ddpg_data)
+    ppo_df['type']='PPO'
+    ddpg_df['type']='DDPG'
+    data = pd.concat([ppo_df,ddpg_df])
+    draw_df = data.reset_index().melt(id_vars=['index', 'type'], var_name='col')
+    
+    sns.set_theme(style='darkgrid')
+    plt.figure(figsize=(7,5))
+    sns_plot=sns.lineplot(  x='index', 
+                            y='value',
+                            hue='type', 
+                            #palette=['b','r'],
+                            data=draw_df)
+    sns_plot.set_xlabel("Epochs")                        
+    sns_plot.set_ylabel("Reward")
+    sns_plot.set(title="Training Curve for PPO and DDPG")
+    sns_plot.figure.savefig( os.path.join(os.path.dirname( __file__ ),f"{fig_title}.png"))
+    print("done")
+
+def train_ppo_ddpg_side():
+    fig_title = "PPO_DDPG_SIDE"
+
+    dir_name = 'ppo_test_amd'
+    case_names = ['2021-12-19_15-00-57-ppo_s0_outliner','2021-12-21_11-09-31-ppo_s0_outliner']
+    ppo_file_name = []
+    for case_name in case_names:
+        ppo_file_name += get_csv(dir_name,case_name)
+    
+    dir_name = 'ddpg_test_amd'
+    case_names = ['2021-12-19_10-11-01-ddpg_s0_t5', '2021-12-19_01-15-44-ddpg_s0_should_t5']
+    ddpg_file_name = []
+    for case_name in case_names:
+        ddpg_file_name += get_csv(dir_name,case_name)
+    
+    ppo_data  = get_min_data(ppo_file_name,set_min=250)
+    ddpg_data = get_min_data(ddpg_file_name,set_min=250)
+
+    ppo_df  = pd.DataFrame(data=ppo_data,index=np.arange(ppo_data.shape[0]),columns=[f"data" for i in range(len(ppo_file_name))])
+    ddpg_df = pd.DataFrame(data=ddpg_data,index=np.arange(ddpg_data.shape[0]))
+    
+    sns.set_theme(style='darkgrid')
+    fig, axes = plt.subplots(1, 2,sharey=True,figsize=(7,5))
+    fig.suptitle("Training Curve for PPO and DDPG")
+    sns.lineplot(data=ppo_df,legend=False,ax=axes[0])
+    sns.lineplot(data=ddpg_df,legend=False,ax=axes[1])
+    axes[0].set_title("PPO")
+    axes[0].set_ylabel("Reward")
+    axes[0].set_xlabel("Epochs")
+    axes[1].set_title("DDPG")
+    axes[1].set_xlabel("Epochs")
+    fig.savefig(os.path.join(os.path.dirname( __file__ ),f"{fig_title}.png"))
+    print("done")
+
 if __name__ == "__main__":
-    liver_p_to_p()
+    #liver_p_to_p()
     #pure_p_to_p()
     #train_ddpg()
     #train_ppo()
+    train_ppo_ddpg_sep()
+    train_ppo_ddpg_group()
+    train_ppo_ddpg_side()
    
